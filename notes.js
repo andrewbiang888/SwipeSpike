31a088caa78d09420b032636dffe958b2b2c0a60
Griff Kennedy <griff.kennedy@ampf.com>
2016-03-24 13:52:59 -0500

    committing swipe spike for first time

---
+++ notes.js
@@ -0,0 +1,12 @@
+﻿function loadNotes() {
+    var html = '';
+    for (var i = 0; i < notesList.length; i++) {
+        html += '<div class="drag-container" style="position:relative;">';
+        html += '    <div id="note_' + notesList[i].id + '" class="swipe-target" style="position:absolute">';
+        html += '        <div class="mobile-note-title" style="position:relative;">' + notesList[i].title + '</div>';
+        html += '        <div class="mobile-note-text" style="position:relative;">' + notesList[i].body + '</div>';
+        html += '    </div>';
+        html += '</div>';
+    }
+    $('#notesList').html(html);
+}
---
+++ swipe.js
@@ -0,0 +1,76 @@
+﻿
+
+    //var jqswipees = $('.swipe-target');
+var swipee = null;//document.getElementById('swipeTarget');
+var swipeeId = null;
+var swipeeLeft = null;
+    //var xup = $('#x_up');
+    //var xdown = $('#x_down');
+    //var xdiff = $('#x_diff');
+    //var left = $('#note_left');
+    //left.text(swipeeLeft);
+
+    var xDown = null;
+    var xDiff = null;
+
+    var notesContainer = document.getElementById('notesList');
+    notesContainer.addEventListener('touchstart', handleTouchStart, false);
+
+    notesContainer.addEventListener('touchend', handleTouchEnd, false);
+
+    notesContainer.addEventListener('touchmove', handleTouchMove, false);
+
+    function handleTouchStart(evt) {
+        swipee = evt.touches[0].target;
+        if (swipee.className === 'swipe-target') {
+            swipeeId = swipee.id;
+            var swipeePos = $('#' + swipeeId).position();
+            swipeeLeft = swipeePos.left;
+            xDown = evt.touches[0].clientX;
+            evt.preventDefault();
+        }
+    };
+
+    function handleTouchEnd(evt) {
+        if (xDiff > 50) showDeleteConfirmation();
+
+        //var upx = evt.touches[0].clientX;
+        //alert('upx='+upx);
+        //$('#x_up').text(xUp);
+
+        evt.preventDefault();
+    }
+
+    function handleTouchMove(evt) {
+        var xPos = evt.touches[0].clientX;
+        xDiff = xDown - xPos;
+        var left = (swipeeLeft - xDiff) + 'px';
+        if (xPos < xDown) {
+            $('#' + swipeeId).css('left', left);
+            //xdiff.text(xDiff);
+            //swipee.css('left', swipeeLeft - xDiff);
+        }
+
+        //$('#x_diff').html(swipeeLeft + xPos);
+        evt.preventDefault();
+    }
+
+    function deleteNote() {
+        hideDeleteConfirmation();
+        $('#'+ swipeeId).css('left', '0px');
+        alert('Note has been deleted.')
+
+    }
+    function cancelDelete() {
+        hideDeleteConfirmation();
+        $('#' + swipeeId).css('left', '0px');
+    }
+
+    function showDeleteConfirmation() {
+        $('#deleteConfirmation').removeClass('hidden');
+    }
+
+    function hideDeleteConfirmation() {
+        $('#deleteConfirmation').addClass('hidden');
+    }
+
